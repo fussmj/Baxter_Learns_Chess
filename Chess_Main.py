91adf0f56dd6c7214d27a18034206a5ec59a9c74
fussmj <fuss.matthew.j@gmail.com>
2021-12-01 12:12:16 -0500

    dfdsg

--- Chess_Main.py
+++ Chess_Main.py
@@ -1,27 +1,53 @@
 from stockfish import Stockfish
 
-import boardClasses
-import modelClasses
-import moveClasses
+
+from boardClasses import Board, Square
+from moveClasses import pick, place
+
+
+#the board length in meters
+board_length = 0.46
+#the x and y coordinates used for the gripper to reach the a1 spot on the chess board
+x_ref = 0.688
+y_ref = -0.652
+z_ref = -0.170
+x_orientation = 0.955
+y_orientation = 0.292
+z_orientation = 0.0
+
+
 
 #initialize the stockfish game
 stockfish = Stockfish("C:/Users/Matthew/Desktop/Baxter_Learns_Chess/stockfish_14.1_win_x64_avx2.exe")
 stockfish.set_position([])
 
-#initialize the robot interface classes
-
+#initialize the robot interface and other classes
+game = Board(x_ref, y_ref, board_length)
 
 print(stockfish.get_board_visual())
 
 while(1):
-
     #white moves first. The text input will be replaced with camera move detection
-    white_move = input("enter white move: \n")
-    stockfish.make_moves_from_current_position([white_move])
+    valid = False
+    while not valid:
+        white_move = input("enter white move: \n")
+        valid = stockfish.is_move_correct(white_move)
+        if not valid:
+            print("Illegal move. please do something else")
+        else:
+            stockfish.make_moves_from_current_position([white_move])
+            old_pose = white_move[0:1]
+            new_pose = white_move[2:3]
+            game.move_piece(old_pose, new_pose)
+
     print(stockfish.get_board_visual())
 
 
     black_move = stockfish.get_best_move_time(2000)
     stockfish.make_moves_from_current_position([black_move])
+    old_pose = white_move[0:1]
+    new_pose = white_move[2:3]
+    game.move_piece(old_pose, new_pose)
+
     print(black_move)
     print(stockfish.get_board_visual())
--- boardClasses.py
+++ boardClasses.py
@@ -1,4 +1,3 @@
-
 import argparse
 import struct
 import sys
@@ -29,37 +28,85 @@ from baxter_core_msgs.srv import (
 
 import baxter_interface
 
+import moveClasses
+
 class Piece:
 
-    def __init__(self, piece_type, pose):
-        self.piece_type = piece_type
+    def __init__(self, pose):
         self.pose = pose
 
 
 class Square:
 
-    def __init__(self, x, y):
+    def __init__(self, pose, x, y):
+        self.pose = pose
         self.x = x
         self.y = y
 
-
 class Board:
 
-    def __init__(self, x_ref, y_ref, board_len):
+    def __init__(self, x_ref, y_ref, board_length):
+        #the x and y ref variables should pertain to the coordinates that the gripper has to be in to reach spot a1 on the board
         self.x_ref = x_ref
         self.y_ref = y_ref
-        self.board_len - board_len
-        self. squares = []
+        hover_distance = 0.15
+        self.pnp = moveClasses.PickAndPlace('right', hover_distance)
+        self.board_length = board_length
+        self.square_length = self.board_length/8
+        self.squares = []
+        self.pieces = []
+        self.overhead_orientation = Quaternion(
+                             x=-0.0249590815779,
+                             y=0.999649402929,
+                             z=0.00737916180073,
+                             w=0.00486450832011)
+        self.dumpster_pose = Pose(position=Point(x=x_ref - 0.2, y=y_ref - 0.3, z=-0.17), orientation=self.overhead_orientation)
+
+        rows = ['1', '2', '3', '4', '5', '6', '7', '8']
+        cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
         for i in range(8):
-            self.squares.append([])
-
-
-
-
-            '''
-            new_x = self.x_ref + i*board_len/8
             for j in range(8):
-                new_y = self.y_ref + j*board_len/8
-                self.squares.append(Pose(position=Point(x=new_x, y=new_y, z=-0.129), orientation=overhead_orientation))
-            '''
+                self.squares.append(Square((cols[i] + rows[j]), x_ref - j * self.square_length, y_ref + i * self.square_length ))
+
+
+        for i in range(8):
+            self.pieces.append(Piece(cols[i] + '1'))
+            self.pieces.append(Piece(cols[i] + '2'))
+            self.pieces.append(Piece(cols[i] + '7'))
+            self.pieces.append(Piece(cols[i] + '8'))
+
+    def move_piece(self, old_pose, new_pose):
+        for piece in self.pieces:
+            if piece.pose == new_pose:
+                for square in self.squares:
+                    if square.pose == piece.pose:
+                        x_pose = square.x
+                        y_pose = square.y
+                taken_piece_pose = Pose(
+                    position=Point(x=x_pose, y=y_pose, z=-0.17),
+                    orientation=self.overhead_orientation)
+                self.pnp.pick(taken_piece_pose)
+                self.pnp.place(self.dumpster_pose)
+                del piece
+        for piece in self.pieces:
+            if piece.pose == old_pose:
+                piece.pose = new_pose
+                for square in self.squares:
+                    if square.pose == old_pose:
+                        old_x = square.x
+                        old_y = square.y
+                    elif square.pose == new_pose:
+                        new_x = square.x
+                        new_y = square.y
+                pick_pose = Pose(position=Point(x=old_x, y=old_y, z=-0.17), orientation=self.overhead_orientation)
+                place_pose = Pose(position=Point(x=new_x, y=new_y, z=-0.17), orientation=self.overhead_orientation)
+                self.pnp.pick(pick_pose)
+                self.pnp.place(place_pose)
+
+
+
+
+
+
+
