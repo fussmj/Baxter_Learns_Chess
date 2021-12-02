5607c8a419647070cb9a11e85b4d9fdaac5d4e30
fussmj <fuss.matthew.j@gmail.com>
2021-12-02 12:36:48 -0500

    cool stuff

--- Chess_Main.py
+++ Chess_Main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
-from stockfish import Stockfish
+
 from boardClasses import Board, Square
 from moveClasses import pick, place
+import socket
 
+#for sending
+UDP_IP = "192.168.1.10"
+UDP_PORT = 5005
+MESSAGE = b"Hello, World!"
+
+#for receiving
+UDP_IP2 = "192.168.1.10"
+UDP_PORT2 = 5005
+sock = socket.socket(socket.AF_INET,  # Internet
+socket.SOCK_DGRAM)  # UDP
+sock.bind((UDP_IP2, UDP_PORT2))
 
 #the board length in meters
 board_length = 0.46
-#the x and y coordinates used for the gripper to reach the a1 spot on the chess board
+#the coordinates used for the gripper to reach the a1 spot on the chess board
 x_ref = 0.688
 y_ref = -0.652
 z_ref = -0.170
@@ -14,36 +26,24 @@ x_orientation = 0.955
 y_orientation = 0.292
 z_orientation = 0.0
 
-
-
-#initialize the stockfish game
-stockfish = Stockfish("C:/Users/Matthew/Desktop/Baxter_Learns_Chess/stockfish_14.1_win_x64_avx2.exe")
-stockfish.set_position([])
-
 #initialize the robot interface and other classes
 game = Board(x_ref, y_ref, board_length)
 
-print(stockfish.get_board_visual())
-
 while(1):
     #white moves first. The text input will be replaced with camera move detection
-    valid = False
-    while not valid:
-        white_move = input("enter white move: \n")
-        valid = stockfish.is_move_correct(white_move)
-        if not valid:
-            print("Illegal move. please do something else")
-        else:
-            stockfish.make_moves_from_current_position([white_move])
-            old_pose = white_move[0:1]
-            new_pose = white_move[2:3]
-            game.move_piece(old_pose, new_pose)
-
-    print(stockfish.get_board_visual())
+    white_move = input("make move\n")
+    MESSAGE = white_move
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    old_pose = white_move[0:1]
+    new_pose = white_move[2:3]
+    game.move_piece(old_pose, new_pose)
 
 
-    black_move = stockfish.get_best_move_time(2000)
-    stockfish.make_moves_from_current_position([black_move])
+    #black_move = stockfish.get_best_move_time(2000)
+    while True:
+        data, addr = sock.recvfrom(1024)
+    black_move = addr
     old_pose = white_move[0:1]
     new_pose = white_move[2:3]
     game.move_piece(old_pose, new_pose)
